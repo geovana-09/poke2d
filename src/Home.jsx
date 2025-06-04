6122ec2781fe68d47b39253b1314b531fa45ff21
geovana-09 <rm95029@estudante.fieb.edu.br>
2025-06-04 11:13:21 +0000

    grid

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,10 +2,12 @@ import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg'
 import './PokeFlex.css'
 import PokeFlex from './PokeFlex'
+import PokeGrid from './Pokegrid'
 const PokeFlex = () => {
     return (
         <div>
            <PokeFlex />
+           <PokeGrid/>
         </div>           
     )
 }
